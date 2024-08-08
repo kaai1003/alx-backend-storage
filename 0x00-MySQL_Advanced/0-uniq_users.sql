4b4d11f57679ade91bcb6d53669f0f7721e675b6
AIMAD KACEM <kacem.aimad@gmail.com>
2024-08-08 01:31:05 +0100

    0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- create table users if not exist
 -- fields: id(in), email(string), name(string)
-CREATE TABLE users IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
