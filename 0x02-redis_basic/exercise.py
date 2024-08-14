efd665087f50ae0337983276bf4164c9365f60fc
AIMAD KACEM <kacem.aimad@gmail.com>
2024-08-15 00:25:17 +0100

    task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Cache class module"""
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """Cach class definition"""
+    def __init__(self) -> None:
+        """redis initialisation"""
+        self._redis = redis.Redis()
+        self._redis.flushdb(True)
+
+    def store(self, data: Union[int, float, str, bytes]) -> str:
+        """set uuid key"""
+        uuid_key = str(uuid.uuid4())
+        self._redis.set(uuid_key, data)
+        return uuid_key
