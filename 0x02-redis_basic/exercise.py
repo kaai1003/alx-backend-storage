fc135cb59004d6002e7229c87c5f5a371cfaef3a
AIMAD KACEM <kacem.aimad@gmail.com>
2024-08-16 23:23:02 +0100

    task2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,3 +17,20 @@ class Cache:
         uuid_key = str(uuid.uuid4())
         self._redis.set(uuid_key, data)
         return uuid_key
+
+    def get(self, key: str,
+            fn: Callable = None) -> Any:
+        """convert the data back to the desired format"""
+        data = self._redis.get(key)
+        if data is None:
+            return
+        if fn is None:
+            return data
+        return fn(data)
+
+    def get_str(self, data: str) -> str:
+        """str conversion function"""
+        return data.decode('utf-8')
+
+    def get_int(self, data: str) -> int:
+        return int(data)
